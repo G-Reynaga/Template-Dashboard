174641442ceb16814c9c1a9ecd9d308e55b86c78
Gerson Moises Reyaga Sotelo <gerson.reynaga23@gmail.com>
2024-04-27 22:51:44 -0500

    config-packages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js'
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-};
+  plugins: [animations, require('flowbite/plugin')({
+    charts: true
+  })]
+}
